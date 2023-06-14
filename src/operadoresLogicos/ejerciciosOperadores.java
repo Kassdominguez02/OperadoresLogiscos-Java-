1bb9a4aed7a541bbbf247698ec7356a20e50d1df
Kassandra Dominguez <kassdomlara02@gmail.com>
2023-06-14 09:01:22 -0600

    Se crea archivo de operadores logicos en Java

---
+++ src/operadoresLogicos/ejerciciosOperadores.java
@@ -0,0 +1,35 @@
+package operadoresLogicos;
+
+public class ejerciciosOperadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * los operadores logicos se utilizan para combinar valores booleanos y nos
+		 * devuelven un resulatdo verdadero, falso
+		 */
+		// && and (y)
+		// ||
+		// ! not
+
+		int a = 5;
+		int b = 3;
+
+		System.out.println(a == b && a > b);
+
+		// Determinar si un num es par
+
+		int num = 7;
+
+		boolean resultado = (num % 2 == 0);
+
+		if (resultado) {
+			System.out.println("el numero es par");
+
+		} else {
+			System.out.println("el numero no es par");
+		}
+
+	}
+}
